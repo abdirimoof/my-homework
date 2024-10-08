2bb1bbd799fe62fff37889b61f68e06e36318d31
abdirimoof <101799444+abdirimoof@users.noreply.github.com>
2024-10-09 00:32:46 +0500

    firs project
    it my first project which i add in github

---
+++ lesson_6/temp.py
@@ -0,0 +1,12 @@
+#ozgaruvchi = int(input("Yangi son kiriting: \n>>>"))
+#print(f"{str(ozgaruvchi)} sonining kvadrati {ozgaruvchi ** 2} ga teng" )
+#print(f"{str(ozgaruvchi)} sonining kubi {ozgaruvchi ** 3} ga teng" )
+#new_item = int(input('Tugilgan yilingizni ayting iloji bulsa: \n>>>'))
+#your_age = 2024 - new_item
+#print(' Mening yoshim ' + str(your_age) + "da1998")
+first_item = int(input('Birinchi sonni kiriting: \n>>>'))
+secand_item = int(input('Ikkinchi sonni kiriting: \n>>>'))
+print(str(first_item) + " + " + str(secand_item) + " = ", first_item + secand_item)
+print(str(first_item) + " * " + str(secand_item) + " = ", first_item * secand_item)
+print(str(first_item) + " // " + str(secand_item) + " = ", first_item // secand_item)
+print(str(first_item) + " % " + str(secand_item) + " = ", first_item % secand_item)
