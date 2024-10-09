768d3570ff5101a87fa282990c71476c4a9baa6a
abdirimoof <101799444+abdirimoof@users.noreply.github.com>
2024-10-10 01:04:07 +0500

    forgacha

--- lesson_6/temp.py
+++ lesson_6/temp.py
@@ -1,12 +1,83 @@
-ozgaruvchi = int(input("Yangi son kiriting: \n>>>"))
-print(f"{str(ozgaruvchi)} sonining kvadrati {ozgaruvchi ** 2} ga teng" )
-print(f"{str(ozgaruvchi)} sonining kubi {ozgaruvchi ** 3} ga teng" )
-new_item = int(input('Tugilgan yilingizni ayting iloji bulsa: \n>>>'))
-your_age = 2024 - new_item
-print(' Mening yoshim ' + str(your_age) + "da1998")
-first_item = int(input('Birinchi sonni kiriting: \n>>>'))
-secand_item = int(input('Ikkinchi sonni kiriting: \n>>>'))
-print(str(first_item) + " + " + str(secand_item) + " = ", first_item + secand_item)
-print(str(first_item) + " * " + str(secand_item) + " = ", first_item * secand_item)
-print(str(first_item) + " // " + str(secand_item) + " = ", first_item // secand_item)
-print(str(first_item) + " % " + str(secand_item) + " = ", first_item % secand_item)
+#ozgaruvchi = int(input("Yangi son kiriting: \n>>>"))
+#print(f"{str(ozgaruvchi)} sonining kvadrati {ozgaruvchi ** 2} ga teng" )
+#print(f"{str(ozgaruvchi)} sonining kubi {ozgaruvchi ** 3} ga teng" )
+#new_item = int(input('Tugilgan yilingizni ayting iloji bulsa: \n>>>'))
+#your_age = 2024 - new_item
+#print(' Mening yoshim ' + str(your_age) + "da1998")
+#first_item = int(input('Birinchi sonni kiriting: \n>>>'))
+#secand_item = int(input('Ikkinchi sonni kiriting: \n>>>'))
+#print(str(first_item) + " + " + str(secand_item) + " = ", first_item + secand_item)
+#print(str(first_item) + " * " + str(secand_item) + " = ", first_item * secand_item)
+#print(str(first_item) + " // " + str(secand_item) + " = ", first_item // secand_item)
+#print(str(first_item) + " % " + str(secand_item) + " = ", first_item % secand_item)
+cars = ['volvo', 'mers', 'shevralet', 'daewo', 'bmw', 'tesla', 'byd']
+#print(cars)
+#cars.sort();
+#print(cars)
+#cars.sort(reverse = True)
+#print(sorted(cars, reverse = True))
+#ran_num = [67, 34, 673, -1651, -34.4, 1, 5]
+#print(sorted(ran_num, reverse=True))
+#print(len(ran_num))
+#sonlar = list(range(1, 11))
+#print(sonlar)
+#juft_sonlar = list(range(0, 21, 2))
+#print(juft_sonlar)
+#max_son = max(sonlar)
+#print(max_son)
+#umumiy = sum(sonlar)
+#print(umumiy)
+#print(cars[0:4])
+#print(cars)
+#my_cars = cars[:]
+#print(my_cars)
+#my_cars.remove('volvo')
+#print(my_cars)
+#print(cars)
+countries = ['uzb', 'ru', 'us', 'uk', 'jap', 'ca']
+print(countries)
+print(len(countries))
+print(sorted(countries))
+print(sorted(countries, reverse = True))
+print(countries)
+countries.reverse()
+print(countries)
+countries.sort()
+print(countries)
+countries.sort(reverse=True)
+print(countries)
+juft = list(range(120, 1200, 2))
+print(juft)
+print('sonlar summasi: ' + str(sum(juft)))
+print('eng katta va eng kichkina farqi: ' + str(max(juft) - min(juft)))
+print("nechta son: " + str(len(juft)))
+print(juft[0:20])
+print( juft[-21:-1])
+dishes = ['osh', 'baliq', 'shorva', 'shashlik', 'tandir']
+breakfast = dishes[:]
+print(breakfast)
+breakfast.append('tuxum')
+breakfast.append('kalbassa')
+print(breakfast)
+breakfast.remove('osh')
+del breakfast[0]
+print(breakfast)
+print(dishes)
+breakfast = tuple(breakfast)
+
+dishes = tuple(dishes)
+print(type(dishes))
+breakfast[0] = 'qaymoq'
+
+
+
+
+
+
+
+
+
+
+
+
+
